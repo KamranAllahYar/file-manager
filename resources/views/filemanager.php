739f08f404b14defebc9ae0ef124c5f5a843000c
Kamran <kamranallahyar@outlook.com>
2023-11-23 12:31:09 +0500

    fixes

--- resources/views/filemanager.php
+++ resources/views/filemanager.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="{{ asset('vendor/file-manager/css/file-manager.css') }}">
+    <link rel="stylesheet" href="{{ asset('/vendor/file-manager/css/file-manager.css') }}">
 </head>
 
 <body>
@@ -17,7 +17,7 @@
         <div id="fm"></div>
     </div>
 
-     <script src="{{ asset('vendor/file-manager/js/file-manager.js') }}"></script>
+     <script src="{{ asset('/vendor/file-manager/js/file-manager.js') }}"></script>
 </body>
 
 </html>
